7931d440e23bc227752f53a860aa70197956a291
Algy Velu <alagu.3012@gmail.com>
2016-04-29 06:56:29 -0700

    Updated todo

--- server.js
+++ server.js
@@ -3,13 +3,55 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var dt = new Date().toString();
 
+var todo = [{
+	id: 1,
+	description: 'Doctor Appointment',
+	Completed: false
+},
+{
+	id: 2,
+	description: 'Pay Bills',
+	Completed: false
+},
+{
+	id: 3,
+	description: 'Laundry tasks',
+	Completed: true
+}
+];
+
 var middlewr = require('./middleware.js');
 
-app.use(middlewr.logger);
+//app.use(middlewr.logger);
 
-app.get('/', function(req, res) {
+app.get('/', middlewr.logger, function(req, res) {
 	res.send('To do API');
 });
+
+app.get('/todo/:id', function(req, res) {
+	var input = parseInt(req.params.id, 10);
+	var matchedvalue ;
+	//var input = parseInt(inp, 10);
+	console.log('Values for Params '+ input);
+	todo.forEach(function (todo) {
+		if (input === todo.id) {
+			matchedvalue = todo;
+		}
+	});
+	console.log('Match' , typeof matchedvalue);
+	if (typeof matchedvalue === 'undefined'){
+			console.log('Match' , typeof matchedvalue);
+	
+			res.status(404).send();
+		} else {
+			res.json(matchedvalue);
+		}
+});
+
+app.get('/todo', function(req, res) {
+	res.json('TO DO, ', todo);
+});
+
 //console.log(__dirname);
 app.listen(PORT, function() {
 	console.log('Express Listening on PORT' , PORT);
