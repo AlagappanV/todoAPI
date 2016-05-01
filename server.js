fc8aeb88acb05bca6fe1d163649608d1cac9371d
Algy Velu <alagu.3012@gmail.com>
2016-05-01 07:53:00 -0700

    Added underscore

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var dt = new Date().toString();
 var todoNextID = 1;
 var todo = [];
+var middlewr = require('./middleware.js');
 
 app.use(bodyParser.json());
-
-
-var middlewr = require('./middleware.js');
 
 //app.use(middlewr.logger);
 
@@ -19,31 +18,25 @@ app.get('/', middlewr.logger, function(req, res) {
 
 app.post('/', function (req, res) {
 	var body = req.body;
-	console.log('In todo');
+	body = _.pick(req.body, 'status', 'description');
+	console.log('body ' , body);
+	if(!_.isBoolean(body.status) || !_.isString(body.description)) {
+	console.log('In IF');
+	return res.status(400).send();
+	}
 	body.id = todoNextID;
-	todoNextID++;
-	
-	//console.log('body ', body);
+	todoNextID++;	
 	todo.push(body);
-	console.log('todo Length ', todo.length);
-
 	res.json(body);
+
 });
 
 app.get('/todo/:id', function(req, res) {
 	var input = parseInt(req.params.id, 10);
-	var matchedvalue ;
-	//var input = parseInt(inp, 10);
-	console.log('Values for Params '+ input);
-	todo.forEach(function (todo) {
-		if (input === todo.id) {
-			matchedvalue = todo;
-		}
-	});
-	//console.log('Match' , typeof matchedvalue);
-	if (typeof matchedvalue === 'undefined'){
-			//console.log('Match' , typeof matchedvalue);
-	
+	var matchedvalue = _.findWhere(todo, {id: input});
+	console.log('Match' , matchedvalue);
+
+	if (typeof matchedvalue === 'undefined'){	
 			res.status(404).send();
 		} else {
 			res.json(matchedvalue);
@@ -56,5 +49,5 @@ app.get('/todo', function(req, res) {
 
 //console.log(__dirname);
 app.listen(PORT, function() {
-	console.log('Express Listening on PORT' , PORT);
+	console.log('Server Listening on PORT' , PORT);
 });
